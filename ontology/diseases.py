85b94bd634319383308a2d9f9ed3db6fa27fc4ff
Umang Sharma <umang.sharma@elucidata.io>
2025-02-05 19:56:15 +0530

    Refactor ontology processing code for improved readability

--- ontology/diseases.py
+++ ontology/diseases.py
@@ -2,45 +2,88 @@
 import pronto
 import pandas as pd
 from scripts.load import load_file
-from scripts.create_db import add_table_name, read_from_db, save_to_db, add_node_table_name
+from scripts.create_db import (
+    add_table_name,
+    read_from_db,
+    save_to_db,
+    add_node_table_name,
+)
+
+
+def get_synonyms(synonyms):
+    """
+    Converts a collection of synonyms into a semicolon-separated string.
+    """
+    syn = list(synonyms)
+    temp = [str(str(s).split("'")[1]) for s in syn]
+    return ";".join(temp)
+
+
+def get_subclass(term):
+    """
+    Retrieves direct subclass IDs (distance=1) for the given term.
+    """
+    mesh = term.subclasses(with_self=False, distance=1)
+    return [str(m.id) for m in mesh]
+
 
 def disease_nodes(mesh):
-    def get_synonyms(synonyms):
-        syn = list(synonyms)
-        temp = [str(str(s).split("'")[1]) for s in syn]
-        return ';'.join(temp)
+    """
+    Reads the disease ontology terms, extracts relevant node information,
+    and saves the nodes to the database.
+    """
+    data = []
+    for term in mesh.terms():
+        term_id = str(term.id)
+        term_name = str(term.name)
+        term_synonyms = get_synonyms(term.synonyms)
+        term_type = "disease"
+        data.append((term_id, term_name, term_synonyms, term_type))
 
-    df = pd.DataFrame([(str(i.id), str(i.name), get_synonyms(i.synonyms),'disease') for i in mesh.terms()],
-                      columns=['disease_id', 'name', 'synonyms','type'])
-    add_node_table_name(['disease__nodes','disease',';'.join(df.columns),'not_mapped'])
+    df = pd.DataFrame(data, columns=["disease_id", "name", "synonyms", "type"])
+
+    table_columns = ";".join(df.columns)
+    add_node_table_name(["disease__nodes", "disease", table_columns, "not_mapped"])
     save_to_db(df, "disease__nodes")
 
 
 def disease_subclass(mesh):
-    def get_subclass(terms):
-        mesh = terms.subclasses(with_self=False, distance=1)
-        return [str(m.id) for m in mesh]
+    """
+    Reading the disease ontology terms, extracts subclass relationships,
+    and saves the relations to the database.
+    """
+    data = []
+    for term in mesh.terms():
+        target_id = term.id
+        subclass_list = get_subclass(term)
+        data.append((target_id, subclass_list))
 
-    sub_df = pd.DataFrame([(i.id, get_subclass(i)) for i in mesh.terms()],
-                          columns=['disease_target', 'disease_source'])
-    sub_df = sub_df.explode('disease_source')
-    sub_df = sub_df.dropna(subset=['disease_source'])
-    sub_df['relation'] = 'is_a'
+    sub_df = pd.DataFrame(data, columns=["disease_target", "disease_source"])
+    sub_df = sub_df.explode("disease_source")
+    sub_df = sub_df.dropna(subset=["disease_source"])
+    sub_df["relation"] = "is_a"
 
-    node_df = read_from_db("disease__nodes")[['disease_id']]
-    node_df.columns = ['disease_source']
-    sub_df = pd.merge(node_df,sub_df,on='disease_source',how='inner')
+    node_df = read_from_db("disease__nodes")[["disease_id"]]
+    node_df = node_df.rename(columns={"disease_id": "disease_source"})
 
-    add_table_name(["disease__is_a__relation",'disease','disease','is_a','not_mapped'])
-    save_to_db(sub_df, "disease__is_a__relation")
+    merged_df = pd.merge(node_df, sub_df, on="disease_source", how="inner")
+
+    add_table_name(
+        ["disease__is_a__relation", "disease", "disease", "is_a", "not_mapped"]
+    )
+    save_to_db(merged_df, "disease__is_a__relation")
 
 
 def main_mesh_disease(mesh):
+    """
+    Processes the disease [new]ontology.
+    """
     ontology, new = mesh
     if new:
         disease_nodes(ontology)
         disease_subclass(ontology)
 
-# main
-if __name__ == '__main__':
-    main_mesh_disease((pronto.Ontology("ontologies/disease.obo"),True))
+
+if __name__ == "__main__":
+    ontology = pronto.Ontology("ontologies/disease.obo")
+    main_mesh_disease((ontology, True))
